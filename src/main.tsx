426af072c0bdd3ef9268024eb3dbde6bf7e273e6
RealLacuni <71047915+RealLacuni@users.noreply.github.com>
2024-03-25 00:42:55 -0400

    Remove MantineProvider and related styles.css import

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './index.css';
 import App from './App';
-import { MantineProvider } from '@mantine/core';
-import '@mantine/core/styles.css';
 
 // use create root instead
 import { createRoot } from 'react-dom/client';
@@ -11,11 +9,8 @@ import { FilesContextProvider } from './FilesContext';
 const rootElement = document.getElementById('root');
 if (rootElement) {
   createRoot(rootElement).render(
-    <MantineProvider>
       <FilesContextProvider>
       <App />
       </FilesContextProvider>
-
-    </MantineProvider>
   );
 }
