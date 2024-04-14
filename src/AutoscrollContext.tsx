f2911c3cb2079290e89f02454db56b56c1010aac
RealLacuni <71047915+RealLacuni@users.noreply.github.com>
2024-04-14 18:40:40 -0400

    play/pause buttons and functionality

--- src/AutoscrollContext.tsx
+++ src/AutoscrollContext.tsx
@@ -4,25 +4,38 @@ import React from 'react';
 type AutoScrollType = {
     speed: number,
     handleSpeedChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-
+    isPlaying : boolean;
+    playFunction: () => void;
+    backToTopFunction: () => void;
 }
 
 const initialValues = {
     speed: 0,
     handleSpeedChange: () => {},
+    isPlaying: false,
+    playFunction: () => {},
+    backToTopFunction: () => {},
 } as AutoScrollType;
 
 const AutoScrollContext = createContext<AutoScrollType>(initialValues);
 
 function AutoScrollContextProvider({children}: {children: React.ReactNode}) {
     const [speed, setSpeed] = React.useState<number>(0);
+    const [isPlaying, setIsPlaying] = React.useState<boolean>(false);
 
     const handleSpeedChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSpeed(parseInt(e.target.value));
     }
+
+    const playFunction = () => {
+        setIsPlaying(!isPlaying);
+    }
+    const backToTopFunction = () => {
+        window.scrollTo(0,0);
+    }
     
     return (
-        <AutoScrollContext.Provider value={{speed, handleSpeedChange}}>
+        <AutoScrollContext.Provider value={{speed, handleSpeedChange, isPlaying, playFunction, backToTopFunction}}>
             {children}
         </AutoScrollContext.Provider>
     )
--- src/Toolbar/AutoScrollComponent.tsx
+++ src/Toolbar/AutoScrollComponent.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { PlayIcon, PauseIcon, BarsArrowUpIcon } from '@heroicons/react/24/outline';
+import { AutoScrollContext } from '../AutoscrollContext';
 
 
 const AutoScrollComponent = () => {
-
+  const {isPlaying, playFunction, backToTopFunction} = useContext(AutoScrollContext);
   return (
     <>
-        {/* TODO: implement Play, Pause, and Back to Top component buttons*/}
-
+      {/* TODO: implement Play, Pause, and Back to Top component buttons*/}
+{isPlaying ?      <PlayIcon className="w-8 h-8 cursor-pointer border border-slate-400 text-slate-600 bg-slate-300 rounded-sm p-0.5  hover:text-white"
+      onClick={playFunction}/> : 
+      <PauseIcon className="w-8 h-8 cursor-pointer border border-slate-400 text-slate-600 bg-slate-300 rounded-sm p-0.5  hover:text-white"
+      onClick={playFunction}/>}
+      <BarsArrowUpIcon className="w-8 h-8 cursor-pointer border border-slate-400 text-slate-600 bg-slate-300 rounded-sm p-0.5 hover:text-white" 
+      onClick={backToTopFunction}/>
     </>
-  )
-}
+  );
+};
 
-export default AutoScrollComponent
+export default AutoScrollComponent;
